bd3eda19bc9a62edbbcff6ddde68e1223799d9ea
AforArdi <binsbro36@gmail.com>
2026-03-30 06:29:27 +0600

    initial commit, google manrope font, tailwind withoverride, daisykhala, Page ready for designing

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-bold text-5xl border'>Page is Reacy for Assignment</h1>
+      <button className='btn btn-primary'>Click Me</button>
+    </>
+  )
+}
+
+export default App
